90cf9fcae0b5e3733a24b3912d85473511b92a86
Kotori-Minami-XI <543566812@qq.com>
2020-06-08 17:30:12 +0800

    [Dubbo] Demo

---
+++ Dubbo-Provider/src/main/java/com/Kotori/provider/ProviderApp.java
@@ -0,0 +1,18 @@
+package com.Kotori.provider;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ProviderApp {
+    public static void main(String[] args) {
+        try {
+            //加载xml配置文件启动
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:provider.xml");
+            context.start();
+            System.out.println("provider-0已经启动，按任意键退出");
+            System.out.println("——————————————————————————————————————————");
+            System.in.read(); // 按任意键退出
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
