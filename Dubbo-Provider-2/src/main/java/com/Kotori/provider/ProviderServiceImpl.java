9189811416bc11463875d7ae3a5fa5663a5d7937
Kotori-Minami-XI <543566812@qq.com>
2020-06-09 12:20:56 +0800

    [task] Dubbo demo

---
+++ Dubbo-Provider-2/src/main/java/com/Kotori/provider/ProviderServiceImpl.java
@@ -0,0 +1,9 @@
+package com.Kotori.provider;
+
+import com.Kotori.Interface.ProviderService;
+
+public class ProviderServiceImpl implements ProviderService {
+    public String SayHello(String word) {
+        return word+ "Provider-2收到这个调用请求";
+    }
+}
